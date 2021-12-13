7976c9f678cd6e477cbcadefb4fc3b5bb2bd6498
Mateusz Kolodziejczyk <20084190@mail.wit.ie>
2021-12-13 22:23:50 +0000

    Add working bottom navigation menu

--- app/src/main/java/org/mk/playlist/activities/MainActivity.kt
+++ app/src/main/java/org/mk/playlist/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.mk.playlist.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.Navigation.findNavController
@@ -11,26 +12,33 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.navigation.NavController
 
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
 import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.mk.playlist.R
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val navHostFragment: NavHostFragment = supportFragmentManager
-//            .findFragmentById(R.id.main_content) as NavHostFragment
-//        val navController: NavController = navHostFragment.navController
-//        val appBarConfiguration = AppBarConfiguration(navController.graph)
-//
-//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
+        val navHostFragment: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.main_content) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val bottomNavMenu = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavMenu.setupWithNavController(navController)
     }
 
 //    override fun onCreateOptionsMenu(menu: Menu): Boolean {
 //        menuInflater.inflate(R.menu.menu_main, menu)
 //        return super.onCreateOptionsMenu(menu)
 //    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
 }
 
